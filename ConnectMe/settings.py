64ff01a2127990b4c9e35b3f3c4ed562fb155bf6
ajaywagh955 <waghajay304@gmail.com>
2024-04-24 13:21:13 +0530

    first commit

--- ConnectMe/settings.py
+++ ConnectMe/settings.py
@@ -15,8 +15,8 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG","False").lower() == "true"
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS")
-
+# ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS")
+ALLOWED_HOSTS = ["https://connect-me-l4d2.onrender.com","127.0.0.1:8000"]
 
 # Application definition
 
