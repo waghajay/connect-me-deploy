3ed25aaa18bc1acf703dcafe4adb43fca01e8c52
ajaywagh955 <waghajay304@gmail.com>
2024-04-24 22:00:18 +0530

    changed asgi file

--- ConnectMe/asgi.py
+++ ConnectMe/asgi.py
@@ -40,18 +40,20 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 
 import os
 from django.core.asgi import get_asgi_application
-from django.conf import settings
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-import chat.routing
-import call.routing
 
 # Ensure DJANGO_SETTINGS_MODULE is set correctly
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ConnectMe.settings')
 
 # Initialize Django settings before accessing them
+django_asgi_application = get_asgi_application()
+
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+import chat.routing
+import call.routing
+
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_application,
     "websocket": AuthMiddlewareStack(
         URLRouter(
             chat.routing.websocket_urlpatterns +
